a34dd66b249532b71043e4cc780d3e5eed857112
Amirhossein <ahbavand@gmail.com>
2017-01-14 02:38:00 -0800

    bavand commit

---
+++ xv6-public/getppidTest.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main (void)
+{
+ printf(1,"**************************************************\n");
+ printf(1, "the parent process id of current function is : %d\n" );
+ printf(1,"**************************************************\n");
+ exit();
+}
---
+++ xv6-public/trap.c
@@ -0,0 +1,172 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "x86.h"
+#include "traps.h"
+#include "spinlock.h"
+
+
+
+struct proc* queue[MAX];
+int front = 0;
+int rear = -1;
+int itemCount = 0;
+
+struct proc* peek() {
+    return queue[front];
+}
+
+bool isEmpty() {
+    return itemCount == 0;
+}
+
+bool isFull() {
+    return itemCount == MAX;
+}
+
+int size() {
+    return itemCount;
+}
+
+void insert(struct proc* data) {
+
+    if(!isFull()) {
+
+        if(rear == MAX-1) {
+            rear = -1;
+        }
+
+        queue[++rear] = data;
+        itemCount++;
+    }
+}
+
+struct proc* removeData() {
+    struct proc* data = queue[front++];
+
+    if(front == MAX) {
+        front = 0;
+    }
+
+    itemCount--;
+    return data;
+}
+
+
+
+
+// Interrupt descriptor table (shared by all CPUs).
+struct gatedesc idt[256];
+extern uint vectors[];  // in vectors.S: array of 256 entry pointers
+struct spinlock tickslock;
+uint ticks;
+uint co;
+
+void
+tvinit(void)
+{
+  int i;
+
+  for(i = 0; i < 256; i++)
+    SETGATE(idt[i], 0, SEG_KCODE<<3, vectors[i], 0);
+  SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
+
+  initlock(&tickslock, "time");
+}
+
+void
+idtinit(void)
+{
+  lidt(idt, sizeof(idt));
+}
+
+//PAGEBREAK: 41
+void
+trap(struct trapframe *tf)
+{
+  if(tf->trapno == T_SYSCALL){
+    if(proc->killed)
+      exit();
+    proc->tf = tf;
+    syscall();
+    if(proc->killed)
+      exit();
+    return;
+  }
+
+  switch(tf->trapno){
+  case T_IRQ0 + IRQ_TIMER:
+    if(cpunum() == 0){
+      acquire(&tickslock);
+      ticks++;
+       co++;
+      if(proc && proc->state==RUNNING)//my code
+          proc->rtime++;//my code
+      wakeup(&ticks);
+      release(&tickslock);
+    }
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_IDE:
+    ideintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_IDE+1:
+    // Bochs generates spurious IDE1 interrupts.
+    break;
+  case T_IRQ0 + IRQ_KBD:
+    kbdintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_COM1:
+    uartintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + 7:
+  case T_IRQ0 + IRQ_SPURIOUS:
+    cprintf("cpu%d: spurious interrupt at %x:%x\n",
+            cpunum(), tf->cs, tf->eip);
+    lapiceoi();
+    break;
+
+  //PAGEBREAK: 13
+  default:
+    if(proc == 0 || (tf->cs&3) == 0){
+      // In kernel, it must be our mistake.
+      cprintf("unexpected trap %d from cpu %d eip %x (cr2=0x%x)\n",
+              tf->trapno, cpunum(), tf->eip, rcr2());
+      panic("trap");
+    }
+    // In user space, assume process misbehaved.
+    cprintf("pid %d %s: trap %d err %d on cpu %d "
+            "eip 0x%x addr 0x%x--kill proc\n",
+            proc->pid, proc->name, tf->trapno, tf->err, cpunum(), tf->eip,
+            rcr2());
+    proc->killed = 1;
+  }
+
+  // Force process exit if it has been killed and is in user space.
+  // (If it is still executing in the kernel, let it keep running
+  // until it gets to the regular system call return.)
+  if(proc && proc->killed && (tf->cs&3) == DPL_USER)
+    exit();
+
+  // Force process to give up CPU on clock tick.
+  // If interrupts were on while locks held, would need to check nlock.
+  if(proc && proc->state == RUNNING && tf->trapno == T_IRQ0+IRQ_TIMER&&co==QUANTA){
+
+      co=0;
+
+      yield();
+
+  }
+
+    // Check if the process has been killed since we yielded
+
+
+  if(proc && proc->killed && (tf->cs&3) == DPL_USER)
+    exit();
+}
